58b93324af2037582d2fafb9c05ddc6d97fee9b6
Tasnuva12 <tasnuvamehnaz@iut-dhaka.edu>
2025-01-28 13:43:46 +0600

    The project created

---
+++ app/src/main/java/com/example/noteapp/HomeScreen.kt
@@ -0,0 +1,15 @@
+package com.example.noteapp
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen(modifier: Modifier){
+    Column(modifier=modifier.padding(start = 16.dp, top = 20.dp, end = 16.dp)) {
+
+    }
+}
